6e840a687040976ace2f83dd0403cb1456d8b462
FDOURL28 <francois.dourlensmonchy@decathlon.com>
2021-02-02 08:41:44 +0100

    Start

---
+++ src/app/pokemons/pokemons.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonsListComponent } from './pokemons-list/pokemons-list.component';
+
+
+@NgModule({
+  declarations: [
+    PokemonsListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PokemonsModule { }
