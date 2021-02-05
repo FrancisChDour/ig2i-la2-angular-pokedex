35c0a436941aadfbd75453e4f5e4c2492b87be93
FDOURL28 <francois.dourlensmonchy@decathlon.com>
2021-02-05 14:56:44 +0100

    Sound

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -5,6 +5,10 @@ import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import {RouterModule} from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatButton, MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -16,6 +20,10 @@ import {RouterModule} from '@angular/router';
     CommonModule,
     MatListModule,
     MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatChipsModule,
+    MatButtonModule,
     RouterModule
   ]
 })
